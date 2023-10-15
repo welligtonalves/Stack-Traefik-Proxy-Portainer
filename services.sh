0f7b2a7d11cc196245873ce027234a75095dd5dc
Welligton Alves - Linux4Life <welligtonalves.ti@gmail.com>
2023-10-15 19:43:32 +0000

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#!/bin/bash
+####################################################################
+# Script de backup criado por Welligton Alves Analista eulabs  #####
+####################################################################
 
 LOCATION=$(pwd)
 
